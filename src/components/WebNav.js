11a720221b6f583fffe73b3c9910cf646e12747a
Luis Pirela <34918215+contrastereo@users.noreply.github.com>
2021-10-06 19:07:23 -0300

    Update WebNav.js
    fixing routing issue

--- src/components/WebNav.js
+++ src/components/WebNav.js
@@ -1,24 +1,31 @@
 //dependencies
 import React from "react";
+import { useLocation } from "react-router";
 //bootstrap
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const WebNav = () => {
+  const location = useLocation();
   return (
-    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" >
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">My Hero Alkademy</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        (<Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto d-flex">
-            <Link className="nav-item px-2" to="/">My Team</Link>
-            <Link className="nav-item px-3" to="/search">Search</Link>
-          </Nav>
+          {location.pathname !== "/login" ? (
+            <Nav className="me-auto d-flex">
+              <Link className="nav-item px-2" to="/">
+                My Team
+              </Link>
+              <Link className="nav-item px-3" to="/search">
+                Search
+              </Link>
+            </Nav>
+          ) : null}
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
   );
 };
 export default WebNav;
